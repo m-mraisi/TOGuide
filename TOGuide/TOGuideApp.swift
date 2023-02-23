66aa8a7c16600565e47d1db38fb62a86dd29ef99
m-mraisi <mahmoudmraisi2@gmail.com>
2023-02-23 17:06:21 -0500

    Initial Commit

---
+++ TOGuide/TOGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOGuideApp.swift
+//  TOGuide
+//
+//  Created by Mahmoud Mraisi on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TOGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
