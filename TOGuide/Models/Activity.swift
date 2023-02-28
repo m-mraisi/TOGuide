cbd2a74e4436ff66e68083442772d66a69aaa81f
m-mraisi <mahmoudmraisi2@gmail.com>
2023-02-28 18:53:26 -0500

    finished the ActivityDetailView

--- TOGuide/Models/Activity.swift
+++ TOGuide/Models/Activity.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-class Activity: Identifiable{
+class Activity: Identifiable, Codable{
     var id = UUID()
     var name: String
     var description :String
-    var rating : Double
+    var rating : Int
     var hostName :String
     var images: [String]
     var price :Double
-    
+    var phone: String
     
     init(){
         self.name = "NA"
@@ -24,15 +24,21 @@ class Activity: Identifiable{
         self.hostName = "NA"
         self.images = ["NA"]
         self.price = 0
+        self.phone = "(000)-000-0000"
     }
     
-    init(name: String, description: String, rating: Double, hostName: String, images: [String], price: Double) {
+    init(name: String, description: String, rating: Int, hostName: String, images: [String], price: Double, phone:String) {
         self.name = name
         self.description = description
         self.rating = rating
         self.hostName = hostName
         self.images = images
         self.price = price
+        self.phone = phone
+    }
+    
+    func displayInfo(){
+        print("id: \(id), Name: \(name), Rating: \(rating), Hostname: \(hostName), Price: \(price), Phone: \(phone)")
     }
     
 }
--- TOGuide/Views/ActivityDetailView.swift
+++ TOGuide/Views/ActivityDetailView.swift
@@ -5,15 +5,120 @@
 //
 
 import SwiftUI
+import URLImage
 
 struct ActivityDetailView: View {
     
-    var activity : Activity
+    @EnvironmentObject var dataSource: DataSource
+    @State private var isFavorite = false
+    
+    let activity: Activity
+    
     
     var body: some View {
-        Text("Activity Detail View")
+        VStack {
+            Text(activity.name)
+                .font(.title)
+                .padding(.top, 20)
+            
+            HStack(spacing: 10) {
+                URLImage(URL(string: activity.images[0])!) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150, height: 150)
+                }
+                URLImage(URL(string: activity.images[1])!) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150, height: 150)
+                }
+            }
+            
+            Text(activity.description)
+                .font(.body)
+                .multilineTextAlignment(.leading)
+                .padding(.top, 20)
+                .padding(.horizontal, 10)
+                
+            
+            HStack(spacing: 5) {
+                ForEach(0..<Int(activity.rating)) { index in
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                }
+                ForEach(0..<Int(5-activity.rating)) { index in
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding(.top, 10)
+            
+            HStack {
+                Spacer()
+                Button(action: {
+                    let tel = "tel://"
+                    let formattedString = tel + self.activity.phone //.removingAllWhitespaces()
+                    guard let url = URL(string: formattedString) else { return }
+                    UIApplication.shared.open(url)
+                }) {
+                    Text(activity.phone)
+                        .foregroundColor(.blue)
+                }
+                Spacer()
+            }
+            .padding(.top, 20)
+            
+            HStack {
+                
+                Spacer()
+                ShareLink(item:"Check out this amazing activity! Name: \(activity.name), Price: $\(String(format: "%.2f", activity.price))"){
+                    Image(systemName: "square.and.arrow.up")
+                    Text("Share")
+                }
+                .padding()
+                .background(Color.accentColor)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding(.bottom, 20)
+                
+                Button(action: {
+                    
+                    for item in dataSource.favoriteList{
+                        print(item.displayInfo())
+                    }
+                    
+                    print(dataSource.favoriteList.firstIndex(where: { $0.name == activity.name }) == nil)
+                    
+                    if isFavorite {
+                        dataSource.removeFavoriteActivity(activity)
+                        
+                        
+                    } else {
+                        dataSource.addFavoriteActivity(activity)
+                    }
+                    isFavorite.toggle()
+                }) {
+                    Image(systemName: isFavorite ? "heart.fill" : "heart")
+                    Text("Favorite")
+                }
+                .padding()
+                .background(Color.accentColor)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding(.bottom, 20)
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .onAppear{
+            isFavorite = !(dataSource.favoriteList.firstIndex(where: { $0.name == activity.name }) == nil)
+        }
     }
 }
+
 
 //struct ActivityDetailView_Previews: PreviewProvider {
 //    static var previews: some View {
